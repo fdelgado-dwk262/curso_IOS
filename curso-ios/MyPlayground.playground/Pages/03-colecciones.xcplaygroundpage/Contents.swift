b3c7ef753cc6397763d41b61ad089f9eb9951d78
Equipo 9 <equipo9@GX3WJH6R6D.local>
2026-01-13 13:10:42 +0100

    [Mod] modificaciones

--- curso-ios/MyPlayground.playground/Pages/03-colecciones.xcplaygroundpage/Contents.swift
+++ curso-ios/MyPlayground.playground/Pages/03-colecciones.xcplaygroundpage/Contents.swift
@@ -20,10 +20,11 @@ arrayVacio.append("mundo")
 
 print(arrayVacio)
 
+
 var todosArrays = arrayVacio + nombres
 print(todosArrays)
 
-// nunmeor de items del array
+// numero de items del array
 print(todosArrays.count)
 // posicion concreta del array
 print(todosArrays[2])
@@ -33,6 +34,53 @@ print(todosArrays.count)
 // al usar un array y poner un . depues se habilita la infromación
 // de las operaciones / funciones que podemos tener al respecto
 print(todosArrays.isEmpty)
+// o
+print((todosArrays.count == 0 ? "esta vacio" : "esta lleno"))
 
 print("-----------------------")
 
+// Mas cosas sobre arrays
+// Array de enteros
+var numeros: [Int] = [1, 2, 3, 4, 5]
+
+// Array de cadenas
+var frutas: [String] = ["Manzana", "Banana", "Cereza"]
+
+// Array vacío de doubles
+var valores: [Double] = []
+
+
+//Modificación de elementos
+frutas[1] = "Mango"
+print(frutas) // Output: ["Manzana", "Mango", "Cereza"]
+
+// Agregar elementos
+frutas.append("Naranja")
+print(frutas) // Output: ["Manzana", "Mango", "Cereza", "Naranja"]
+
+// Eliminar elementos
+frutas.remove(at: 2)
+print(frutas) // Output: ["Manzana", "Mango", "Naranja"]
+
+
+print("-----------------------")
+
+//Operaciones comunes con arrays
+
+//Reversar un array:
+let numerosInvertidos = numeros.reversed()
+
+// se necesita convertir la salia a un array con la palabra reservada así nos dará un array invertido
+print(Array(numerosInvertidos))
+// Output: [5, 4, 3, 2, 1]
+
+//Ordenar un array:
+let frutasOrdenadas = frutas.sorted()
+print(frutasOrdenadas)
+
+//Buscar un elemento:
+if frutas.contains("Mango") {
+    print("El array contiene Mango")
+}
+
+print("-----------------------")
